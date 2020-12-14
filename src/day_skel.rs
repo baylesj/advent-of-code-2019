a4b9da3c8f53fcc50219debc22d40ed3fa711a0b
Jordan Bayles <jophba@chromium.org>
2020-12-14 13:00:12 -0800

    Move 2019 to separate repository, unfortunately no history

---
+++ src/day_skel.rs
@@ -0,0 +1,18 @@
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+const INPUT_FILENAME: &'static str = "input/day_.txt";
+
+pub fn part_one() -> i64 {
+    1
+}
+
+pub fn part_two() -> i64 {
+    1
+}
+
+pub fn solve() -> String {
+    format!("part one: {}, part two: {}", part_one(), part_two())
+}
